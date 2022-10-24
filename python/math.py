ec496e04b7dc215d1813a27acf6779da67bd43c6
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-10-24 18:49:10 +0200

    code cleanup

--- python/math.py
+++ python/math.py
@@ -1,16 +1,18 @@
 import numpy as np
 
-# xList = np.array([])
-# zeroThOrder = np.array([])
-# firstOrder = np.array([])
-
 result = []
+xMax = 3.5
+xDelta = 0.25
 
-for x in np.arange(0, 3.5, 0.25):
-    numbers = [x]
-    numbers.append(np.sqrt((2*(1.01325*10**5)*(1.12*10**-3)*x)/(2.045*10**-3)))
-    numbers.append("test")
+#* berekende variabelen voor constanten
+Vmax = None
+conv = None
 
-    result.append(numbers)
+for x in np.arange(0, xMax, xDelta):
+    numbers = [x] # voeg de x posietie in
+    numbers.append(np.sqrt((2*(1.01325*10**5)*(1.12*10**-3)*x)/(2.045*10**-3))) # bereken het 0ste orde model
+    numbers.append("test") # bereken het 1ste orde model
 
-np.savetxt("out.csv", result, delimiter=", ", fmt="% s")
+    result.append(numbers) 
+
+np.savetxt("out.csv", result, delimiter=", ", fmt="% s") # verander de result list in een csv voor importeren.
