930dc95a6a4d341b658fb7889247466501b074eb
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-10-24 18:43:55 +0200

    cleanup

--- python/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-data = [ ["Mando", "Grogu", "Eleven", "Jon", "Ross"],
-         ["Mandalorian", "Mandalorian","Stranger Things", "Game of Thrones", "Friends"],
-         ["Bounty Hunter", "Jedi Master", "Kid", "King", "Paleontologist"],
-         [35, 50, 14, 30, 35] ]
-
-np.savetxt("shows.csv", data, delimiter=", ", fmt="% s")
