a544ed6c163700b94a7839a5a792f716aa40d831
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-10-24 22:16:08 +0200

    λ still borked

--- python/math.py
+++ python/math.py
@@ -14,14 +14,15 @@ atm = 1.01325*10**5
 
 #* berekende extra constanten
 Vmax = np.sqrt((1.01325*10**5)/1.293)
-λ = (2.045*10**-3)/(2.293*(1.13*10**-3))
+λ = np.divide((2.045*10**-3), (2.293*(1.13*10**-3))) #! BORKT
+print(λ)
 
 for x in np.arange(xMin, xMax, xDelta):
     #TODO breek consantes uit
     numbers = [x] # voeg de x posietie in
     numbers.append(np.sqrt((2*(1.01325*10**5)*(1.13*10**-3)*x)/(2.045*10**-3))) # bereken 0ste orde model
     
-    numbers.append(Vmax*np.abs(x/(x+λ)*(1+2*λ/x)**(1/2))) #! bereken 1ste orde model BORKED
+    numbers.append(Vmax*np.absolute(x/(x+λ)*(1+2*λ/x)**(1/2))) # bereken 1ste orde model
     
     result.append(numbers) 
 
