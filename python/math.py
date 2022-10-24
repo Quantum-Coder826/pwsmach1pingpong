c8306c05e34cf1c9d602734c9133a3c41665817c
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-10-24 19:03:51 +0200

    stuff

--- python/math.py
+++ python/math.py
@@ -5,14 +5,16 @@ xMax = 3.5
 xDelta = 0.25
 
 #* berekende variabelen voor constanten
-Vmax = None
-conv = None
+Vmax = np.sqrt((1.01325*10**5)/1.293)
+conv = (2.045*10**-3)/(2.293*(1.13*10**-3))
 
-for x in np.arange(0, xMax, xDelta):
+for x in np.arange(1, xMax, xDelta):
     numbers = [x] # voeg de x posietie in
-    numbers.append(np.sqrt((2*(1.01325*10**5)*(1.12*10**-3)*x)/(2.045*10**-3))) # bereken het 0ste orde model
-    numbers.append("test") # bereken het 1ste orde model
-
+    numbers.append(np.sqrt((2*(1.01325*10**5)*(1.13*10**-3)*x)/(2.045*10**-3))) # bereken het 0ste orde model
+    
+    numbers.append(Vmax*abs(x/(x+conv)*(1+2*conv/x)**(1/2))) # bereken het 1ste orde model
+    
+    print(numbers)
     result.append(numbers) 
 
 np.savetxt("out.csv", result, delimiter=", ", fmt="% s") # verander de result list in een csv voor importeren.
