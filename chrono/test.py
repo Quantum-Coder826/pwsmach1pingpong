dcc754ba1130bd4fd5293979e938ea5cc90f7b73
Berend Veldthuis <57870011+Quantum-Coder826@users.noreply.github.com>
2022-11-23 11:45:42 +0100

    Update test.py

--- chrono/test.py
+++ chrono/test.py
@@ -1,16 +1,20 @@
 # Imports go at the top
 from microbit import *
 
-# setup the pinmodes
-pin0.write_digital(1)
-pin1.write_digital(1)
-
-display.show(Image.YES)
-
 while True:
+    # setup the pinmodes
+    pin0.write_digital(1)
+    pin1.write_digital(1)
+    
+    display.clear()
+    
     # when the a button pressed simulate about a 1.5 ms projectile
     if button_a.is_pressed():
         pin0.write_digital(0)
         sleep(1.5)
         pin1.write_digital(0)
-        display.clear()
+        display.show(Image.YES)
+        while True:
+            if button_b.is_pressed():
+                reset()
+        
