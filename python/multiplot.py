313115fb55a79cfcb4f9fce19edcbc7bb1756aaf
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-12-11 14:59:37 +0100

    werken aan alle plots

--- python/multiplot.py
+++ python/multiplot.py
@@ -27,22 +27,15 @@ Vmax = np.sqrt((1.01325*10**5)/1.293)
 x = np.arange(xMin,xMax + xDelta,xDelta) #maak de lijst van x coordinaden die we willen berekenen, de xMax + XDelta is zodat we wel de xMax halen
 
 # defineer de formules
-y1 = np.sqrt((2*luchtdruk*oppervlak*x)/massa)
-y2 = Vmax*np.absolute(x/(x+λ)*(1+2*λ/x)**(1/2))
+y1 = Vmax*np.absolute(x/(x+λ)*(1+2*λ/x)**(1/2))
 
 # plot de formules
-#plt.plot(x, y1, color='r', label='0th order', marker = 'o')
-plt.plot(x, y2, color='b', label='1st order', marker = 'o')
+plt.plot(x, y1, color='b', label='1st order', marker = 'o')
 
 # extra lijnen voor duidelijkheid
 plt.axhline(343, color='k', linestyle='-', label='geluidssnelheid')
 plt.axhline(Vmax, color='m', linestyle='--', label='Vmax')
 plt.annotate("Vmax = " + str(round(Vmax, 2)), xy=(0,Vmax), textcoords='offset points', xytext=(0,5)) # zet anotation bij de Vmax lijn (-15 voor algemeene plot)
-
-# code om bij elk punt x en y waarde te zetten
-#for i in range(len(x)): # add values to points
-#    plt.annotate( "(" + str(x[i]) + ";" + str(round(y2[i],2)) + ")", xy=(x[i], y2[i]), textcoords='offset points', xytext=(0,5), ha='center')
-
 
 # print de snelheid na 1 meter uit
 for i in range(len(x)):
