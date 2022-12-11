bf313b420bbcbdb3fc5bbaa6484ff28f02cfb38c
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-12-11 12:39:51 +0100

    reoganizing

--- python/plot.py
+++ python/plot.py
@@ -6,14 +6,13 @@ xMin = 0
 xMax = 3.5
 xDelta = 0.125
 
-# vars for math
-massa = 2.045*10**-3
-oppervlak = 0.04267
-
 # wereld constantes
 luchtdruk = 1.01325*10**5
 dichtheidLucht = 1.293
 
+# vars for math
+massa = 2.045*10**-3 # pingpongbal
+oppervlak = 1.13*10-3 # pingpongbal
 
 # berekende extra constanten
 Vmax = np.sqrt((1.01325*10**5)/1.293)
@@ -24,7 +23,7 @@ x = np.arange(xMin,xMax,xDelta)
 y1 = np.sqrt((2*luchtdruk*oppervlak*x)/massa)
 y2 = Vmax*np.absolute(x/(x+λ)*(1+2*λ/x)**(1/2))
 
-#plt.plot(x, y1, color='b', label='0th order', marker = 'o')
+plt.plot(x, y1, color='b', label='0th order', marker = 'o')
 plt.plot(x, y2, color='r', label='1st order', marker = 'o')
 plt.axhline(343, color='k', linestyle='-', label='geluidssnelheid')
 
